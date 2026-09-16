8743b807f0c085d42723bcf1174063147d29e1ce
arjithrs <arjith.rs2025@vitstudent.ac.in>
2026-09-16 12:32:29 +0530

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -1,7 +1,8 @@
-``default_nettype none
+`default_nettype none
 `timescale 1ns / 1ps
 
 module tb ();
+
   initial begin
     $dumpfile("tb.vcd");
     $dumpvars(0, tb);
@@ -17,7 +18,16 @@ module tb ();
   wire [7:0] uio_out;
   wire [7:0] uio_oe;
 
+`ifdef GL_TEST
+  wire VPWR = 1'b1;
+  wire VGND = 1'b0;
+`endif
+
   tt_um_full_adder user_project (
+`ifdef GL_TEST
+      .VPWR(VPWR),
+      .VGND(VGND),
+`endif
       .ui_in  (ui_in),
       .uo_out (uo_out),
       .uio_in (uio_in),
@@ -27,4 +37,5 @@ module tb ();
       .clk    (clk),
       .rst_n  (rst_n)
   );
+
 endmodule
